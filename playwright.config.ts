7fb4c411f60dc9e58dffcfce6bec95b95355267e
maxqa <maxpopow99@mail.ru>
2026-01-22 23:25:23 +0300

    add-new-project-for-visual-testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,9 @@ dotenv.config({ path: '.env.dev' });
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: true,
+  expect: {
+    timeout: 10_000,
+  },
   timeout: 90_000,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
@@ -27,15 +29,40 @@ export default defineConfig({
   },
   
   projects: [
-    {
-      name: 'chromium',
-      use: { 
-        ...devices['Desktop Chrome'],
-        viewport: { width: 1920, height: 1080 }
-      },
-      
 
+    {
+      name: 'functional',
+      testMatch: /^(?!.*visual).*\.spec\.ts$/,
+      use: {
+        browserName: 'chromium',
+        viewport: { width: 1920, height: 1080 },
+      },
     },
+
+
+    // RUN THIS PROJECT ONLY IN DOCKER!
+    // docker run --rm -v "$PWD:/app" pw-tests npx playwright test --project=visual-desktop-chromium
+    {
+      name: 'visual-desktop-chromium',
+      testMatch: /.*visual.*\.spec\.ts$/,
+      fullyParallel: false,
+      workers: 1,
+      use: {
+        browserName: 'chromium',
+        deviceScaleFactor: 1,
+        colorScheme: 'light',
+        locale: 'en-US',
+      },
+    },
+    
+  // {
+    //   name: 'functional',
+    //   testMatch: /^(?!.*visual).*\.spec\.ts$/,
+    //   use: { 
+    //     ...devices['Desktop Chrome'],
+    //     viewport: { width: 1920, height: 1080 }
+    //   },
+    // },
 
     // {
     //   name: 'mobile_chrome',
