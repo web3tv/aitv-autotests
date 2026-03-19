e854b0c2a4694fc4c70b19e271f11c89e48ca9c3
maxqa <maxpopow99@mail.ru>
2026-03-19 16:00:01 +0300

    check2

--- tests/hero/heroIntegration.spec.ts
+++ tests/hero/heroIntegration.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
-import { HeaderPage } from '../src/pages/components/HeaderPage';
-import { MainPage } from '../src/pages/components/MainPage';
+import { HeaderPage } from '../../src/pages/components/HeaderPage';
+import { MainPage } from '../../src/pages/components/MainPage';
 
 test.describe('HERO', () => {
   test('check hero icons', { annotation: { type: 'TC', description: 'HERO-001' } }, async ({ page }) => { 
--- tests/subscription/subscriptionPlan.spec.ts
+++ tests/subscription/subscriptionPlan.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
-import { AuthFlow } from '../src/flows/AuthFlow';
-import { AuthApi } from "../src/api/AuthApi";
-import { SideBarPage } from '../src/pages/components/SideBarPage';
-import { StudioMembershipPage } from '../src/pages/studio/StudioMembershipPage';
-import { UploadVideoFlow } from '../src/flows/UploadVideoFlow';
-import { StudioContentPage } from '../src/pages/studio/StudioContentPage';
-import { StudioProfilePage } from '../src/pages/studio/StudioProfilePage';
-import { ChannelMainPage } from '../src/pages/channel/ChannelMainPage';
-import { VideoPlayerPage } from '../src/pages/components/VideoPlayerPage';
+import { AuthFlow } from '../../src/flows/AuthFlow';
+import { AuthApi } from "../../src/api/AuthApi";
+import { SideBarPage } from '../../src/pages/components/SideBarPage';
+import { StudioMembershipPage } from '../../src/pages/studio/StudioMembershipPage';
+import { UploadVideoFlow } from '../../src/flows/UploadVideoFlow';
+import { StudioContentPage } from '../../src/pages/studio/StudioContentPage';
+import { StudioProfilePage } from '../../src/pages/studio/StudioProfilePage';
+import { ChannelMainPage } from '../../src/pages/channel/ChannelMainPage';
+import { VideoPlayerPage } from '../../src/pages/components/VideoPlayerPage';
 
 test('Paid video suite', { annotation: [{ type: 'TC', description: 'PAID-001' }, { type: 'TC', description: 'PAID-002' }, { type: 'TC', description: 'PAID-003' }] }, async ({ page, request }) => {
     test.setTimeout(180_000);
